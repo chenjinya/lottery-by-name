a69f118bf622dc78f3ba41c54c71852ef2bffcec
Jinya Chen <jinyachen@gmail.com>
2018-01-09 11:36:46 +0800

    update some code

--- README.md
+++ README.md
@@ -3,4 +3,9 @@
 
 代码糙猛快，能用就ok。
 
+## Usage
+`index.html` 主文件，用浏览器打开即可使用
+
+`js/conf.js` 名单列表
+
 MIT
--- js/conf.js
+++ js/conf.js
@@ -1,1 +1,84 @@
-﻿var nameListObj = [{name:"李芳" },{name:"刘亚星" },{name:"肖雨霏" },{name:"勾剑" },{name:"郭力" },{name:"黄惠娜" },{name:"廖杨" },{name:"平媛" },{name:"骞蕙" },{name:"沈萌" },{name:"佟雪" },{name:"王婉" },{name:"王志丹" },{name:"黄晓艺" },{name:"杨通" },{name:"张靖华" },{name:"张梦菲" },{name:"赵阳" },{name:"周达" },{name:"黄思淼" },{name:"郑文迪" },{name:"杨曦" },{name:"李赫颖" },{name:"刘媛媛" },{name:"张囡" },{name:"任文涛" },{name:"张璇" }];
+﻿var config = {};
+config.nameObjectList = [
+    {
+        name: "李芳"
+    },
+    {
+        name: "刘亚星"
+    },
+    {
+        name: "肖雨霏"
+    },
+    {
+        name: "勾剑"
+    },
+    {
+        name: "郭力"
+    },
+    {
+        name: "黄惠娜"
+    },
+    {
+        name: "廖杨"
+    },
+    {
+        name: "平媛"
+    },
+    {
+        name: "骞蕙"
+    },
+    {
+        name: "沈萌"
+    },
+    {
+        name: "佟雪"
+    },
+    {
+        name: "王婉"
+    },
+    {
+        name: "王志丹"
+    },
+    {
+        name: "黄晓艺"
+    },
+    {
+        name: "杨通"
+    },
+    {
+        name: "张靖华"
+    },
+    {
+        name: "张梦菲"
+    },
+    {
+        name: "赵阳"
+    },
+    {
+        name: "周达"
+    },
+    {
+        name: "黄思淼"
+    },
+    {
+        name: "郑文迪"
+    },
+    {
+        name: "杨曦"
+    },
+    {
+        name: "李赫颖"
+    },
+    {
+        name: "刘媛媛"
+    },
+    {
+        name: "张囡"
+    },
+    {
+        name: "任文涛"
+    },
+    {
+        name: "张璇"
+    }
+];
